67aacb38c5a2415b2efa015ba4d55a8f44883200
nonkosii <paynexaba@gmail.com>
2023-11-06 09:57:41 +0200

    final commit

--- resume/settings.py
+++ resume/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
-
+# Development
+SECRET_KEY = 'b@t3l$%#&qdfod%dch9l===f%&!%uudx@7c$c@$-w_p^zdzq&p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -90,9 +91,9 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES = {
+# 	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -131,10 +132,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #For Production
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #For development
-#STATIC_ROOT = '/static/' 
+STATIC_ROOT = '/static/' 
 
 
 STATICFILES_DIRS = [
